212bc69a6a2143b39ccd989e23dc532083f94863
Masakaze <redkingslime@gmail.com>
2017-11-21 00:25:27 +0900

    プリプロセス処理を実装。画像を小さくしたりする処理

--- EyeSensor/EyeSensor.py
+++ EyeSensor/EyeSensor.py
@@ -43,11 +43,44 @@ class EyeSensor:
 
 # end Class EyeSensor
 
+def preprocess(image_file):
+    preprocessed_file = "./Test.jpg"
+
+    # 規格統一のために256x256などにリサイズ
+    import subprocess
+    cmd = "convert -scale 256x256! " + image_file + " " + preprocessed_file
+    subprocess.call(cmd, shell=True)
+
+    # 計算リソースが足りないので当面は16x16ずつの平均カラーを計算して32x32の画像に落とし込むとかする
+    img = Image.open(preprocessed_file)
+    width, height = img.size
+    
+    # 256x256 -> 16x16
+    new_size = (24, 24)
+    new_img = Image.new("RGB", new_size)
+    for x in range(new_size[0]):
+        for y in range(new_size[1]):
+            ave = [0, 0, 0]
+            blocks = (256/new_size[0], 256/new_size[1])
+            ranges = (range(blocks[0]), range(blocks[1]))
+            for i in ranges[0]:
+                for j in ranges[1]:
+                    pix_col = img.getpixel((x*blocks[0]+i, y*blocks[1]+j))
+                    for c in range(3): ave[c] += pix_col[c]
+            for c in range(3): ave[c] = (int)((float)(ave[c])/64)
+            new_img.putpixel((x, y), tuple(ave))
+    new_img.save(preprocessed_file)
+
+    new_img.show()
+
+    return preprocessed_file
+#end def preprocess
+
 if __name__ == '__main__':
     eye_sensor = EyeSensor()
 
-    # 規格統一のために256x256などにリサイズ
+    input_file = "./Apple.jpg"
 
-    # 計算リソースが足りないので当面は16x16ずつの平均カラーを計算して32x32の画像に落とし込むとかする
+    test_file = preprocess(input_file)
 
-    eye_sensor.execute("./Apple.jpg")
+#    eye_sensor.execute(test_file)
