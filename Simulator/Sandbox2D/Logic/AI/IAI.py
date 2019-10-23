2481dbf54b265c8e68516a624cc04f58accae513
Masakaze <redkingslime@gmail.com>
2019-10-24 02:19:20 +0900

    SensorMessageとAIについてTODO・実装案をメモ

--- Simulator/Sandbox2D/Logic/AI/IAI.py
+++ Simulator/Sandbox2D/Logic/AI/IAI.py
@@ -15,6 +15,15 @@ class IAI:
         pass
     # def __init__
 
+    # Actor -> AI -> Componentという階層になりそうなのでAIはcomponentという概念から外したほうが良さそう
+    # Actor.update()
+    #   AI.update()
+    #   for component in components:
+    #     component.update()
+    #   for component in components:
+    #     component.post_update()
+    # StateBase(BehaviorTree)な挙動と、長期的にリスケジュールする処理は分けたほうがいいかも
+    #   後者はやるとしたら別スレッドで余った処理でタイムスライス。適当なタイミングでBehaviorTreeを再構成(or再選択)
     def update(self):
         pass
     # def update
--- Simulator/Sandbox2D/Logic/Eat/IEatable.py
+++ Simulator/Sandbox2D/Logic/Eat/IEatable.py
@@ -46,6 +46,40 @@ class IEatable:
         pass
     # def post_update
 
+    # if is_use_sensor_message_component -> component.receive_msg
+    def is_use_sensor_message_component(self):
+        return True
+    # def is_use_sensor_message_component
+
+    def send_msg(self, sender, receiver):
+        # 食べられたので食べた人に効果メッセージ送信
+        # send_msg_to_host("RecoverLife:10", self.ead_host_info["sender"], ead_host_info["receiver"])
+        #  receiver.get_host.receive_msg(msg, sender, receiver) ??
+        #    for component in components: ret |= component.receive_msg(msg, sender, receiver) ?? send_msg側はどのコンポーネントが答えたかわからないけど、sensor(1とアクターはわかるからいいか)
+        pass
+    # send_msg
+
+    # AI(かアクター層で)
+    #   for component in components:
+    #     ret |= component.receive_msg
+    #   if ret_one:
+    #     if msg == "Eat":
+    #       startAction("Eated")
+    def receive_msg(self, msg, sender, receiver):
+        # Eatableというセンサーがあればそれ、なければBodyをデフォルト反応にするとかかな。。
+        # Componentごとにセンサー持っていて、デフォルトはBody(かPhysics)と同じ値が入るとかのほうが依存は少ないか(処理大きいけど)
+        # ↑そう考えるとcomponentを並列にするよりPhysicsやセンサーはアクター基本設定が上位にあって、下位に渡していくイメージがいいかも
+        # または、RefSensorName : "Body" みたいな感じで参照をデータ化するか
+        if msg == "IsEnableEat":
+            # 食べられますか確認メッセージ 主にUI表示
+            return self.params[self.current].is_enable_eat
+        elif msg == "Eat":
+            # 食べるアクションを受ける
+            # self.eat_host_info = {"sender" : sender, "receiver" : receiver}
+            return self.params[self.current].is_enable_eat
+        # if msg
+    # def receive_msg
+
 # class IEatable
 
 if __name__ == "__main__":
