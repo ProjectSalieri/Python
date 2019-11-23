e8442e9f742a0c8fc28ed4bb3336e0bcc45f8d9c
Masakaze <redkingslime@gmail.com>
2019-11-24 02:24:37 +0900

    TileDrawerの設定フォーマット変更

--- Simulator/Sandbox/GameSense/Graphics/TileDrawer.py
+++ Simulator/Sandbox/GameSense/Graphics/TileDrawer.py
@@ -8,14 +8,15 @@ from .DrawerUtil import DrawerUtil
 class TileDrawer:
 
     def __init__(self):
-        self.tile_info = [[0, 0], [0, 0]]
+        self.tile_info = [["Ice", "Ice"], ["Ice", "Ice"]]
         self.tile_size = (32, 32)
 
-        self.image = None
+        self.images = {}
     # def __init__
 
     def init_from_setting(self, draw_setting):
-        self.image = DrawerUtil.create_image(draw_setting["Image"])
+        for image_key in draw_setting["Images"]:
+            self.images[image_key] = DrawerUtil.create_image(draw_setting["Images"][image_key])
         self.priority = DrawerUtil.get_priority_from_setting(draw_setting)
 
     def set_tile_info(self, tile_info):
@@ -30,7 +31,8 @@ class TileDrawer:
         len_z = len(self.tile_info[0])
         for x in range(len_x):
             for z in range(len_z):
-                screen.blit(self.image, (pos[0] + self.tile_size[0]*x, pos[2] + self.tile_size[1]*z))
+                image = self.images[self.tile_info[x][z]]
+                screen.blit(image, (pos[0] + self.tile_size[0]*x, pos[2] + self.tile_size[1]*z))
             #
         #
     # def draw
